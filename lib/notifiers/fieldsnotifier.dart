75ceeeb81287b068209067ec1898abee4276b7f2
eokdev <emmanuelgbenga14@gmail.com>
2023-09-11 15:11:16 +0100

    eokdev

--- lib/notifiers/fieldsnotifier.dart
+++ lib/notifiers/fieldsnotifier.dart
@@ -4,4 +4,4 @@ final fullname = ValueNotifier<String>("Olorunshola Emmanuel Korede");
 final slackusername = ValueNotifier<String>("Olorunshola Emmanuel");
 final githubhandble = ValueNotifier<String>("eokdev");
 final bio = ValueNotifier<String>(
-    "My name is Olorunshola Emmanuel, and I was born on February 11, 2003, in Nigeria. I am a passionate Flutter developer with a strong foundation in computer science. I pursued my studies in computer science at Joseph Ayo Babalola University.");
+    "My name is Olorunshola Emmanuel, born in Nigeria. I am a passionate Flutter developer with a strong foundation in computer science. I pursued my studies in computer science at Joseph Ayo Babalola University.");
